0cb91fe1619d8140034e9596fe2873f863376a4c
Ivan Molodetskikh <yalterz@gmail.com>
2013-11-11 17:00:23 +0400

    Forgot to change the version number.

--- MouseTweaks/Constants.java
+++ MouseTweaks/Constants.java
@@ -7,7 +7,7 @@ public class Constants
     public static final String  NAME                 = "Mouse Tweaks";
     
     // Mod version
-    public static final String  VERSION              = "2.3.3 (for Minecraft 1.6.2)";
+    public static final String  VERSION              = "2.3.4 (for Minecraft 1.6.4)";
     
     // Obfuscated names
     public static final String  ISMOUSEOVERSLOT_NAME = "a";
