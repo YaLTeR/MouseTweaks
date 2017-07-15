39d5a5f7b1eccb99244a4ed09017adcaffb1a518
Ivan Molodetskikh <yalterz@gmail.com>
2017-07-15 17:16:38 +0300

    Update to Minecraft 1.12

--- src/main/java/yalter/mousetweaks/Constants.java
+++ src/main/java/yalter/mousetweaks/Constants.java
@@ -16,10 +16,10 @@ public class Constants {
 	static final String CONFIG_DEBUG                  = "Debug";
 
 	// Names for reflection.
-	public static final ObfuscatedName IGNOREMOUSEUP_NAME = new ObfuscatedName("ignoreMouseUp", "field_146995_H", "H");
-	public static final ObfuscatedName DRAGSPLITTING_NAME = new ObfuscatedName("dragSplitting", "field_147007_t", "t");
-	public static final ObfuscatedName DRAGSPLITTINGBUTTON_NAME = new ObfuscatedName("dragSplittingButton", "field_146988_G", "G");
-	public static final ObfuscatedName GETSLOTATPOSITION_NAME = new ObfuscatedName("getSlotAtPosition", "func_146975_c", "c");
+	public static final ObfuscatedName IGNOREMOUSEUP_NAME = new ObfuscatedName("ignoreMouseUp", "field_146995_H", "I");
+	public static final ObfuscatedName DRAGSPLITTING_NAME = new ObfuscatedName("dragSplitting", "field_147007_t", "u");
+	public static final ObfuscatedName DRAGSPLITTINGBUTTON_NAME = new ObfuscatedName("dragSplittingButton", "field_146988_G", "H");
+	public static final ObfuscatedName GETSLOTATPOSITION_NAME = new ObfuscatedName("getSlotAtPosition", "func_146975_c", "d");
 	public static final ObfuscatedName HANDLEMOUSECLICK_NAME = new ObfuscatedName("handleMouseClick", "func_184098_a", "a");
 
 	static final String ONTICKMETHOD_FORGE_NAME      = "Forge";
