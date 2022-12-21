045d8857b90fd89d76675bfe17fb225b2323a165
Ivan Molodetskikh <yalterz@gmail.com>
2022-12-20 21:19:20 -0800

    Update to Minecraft 1.19.3
    Fixes https://github.com/YaLTeR/MouseTweaks/issues/96

--- src/main/java/yalter/mousetweaks/ConfigScreen.java
+++ src/main/java/yalter/mousetweaks/ConfigScreen.java
@@ -71,9 +71,8 @@ public class ConfigScreen extends Screen {
                 .create(this.width / 2 - 155, this.height / 6 + 24 * 5, 310, 20,
                         Component.literal("Debug Mode"), (button, value) -> Config.debug = value));
 
-        this.addRenderableWidget(new Button(
-                this.width / 2 - 100, this.height - 27, 200, 20,
-                CommonComponents.GUI_DONE, button -> this.onClose()));
+        this.addRenderableWidget(Button.builder(CommonComponents.GUI_DONE, button -> this.onClose())
+                .bounds(this.width / 2 - 100, this.height - 27, 200, 20).build());
     }
 
     @Override
