ee849e603d92f7cf15e9041dec65db68e3701a38
Ivan Molodetskikh <yalterz@gmail.com>
2025-06-24 21:06:52 +0300

    Update for Forge 56 for 1.21.6

--- src/main/java/yalter/mousetweaks/forge/MouseTweaksForge.java
+++ src/main/java/yalter/mousetweaks/forge/MouseTweaksForge.java
@@ -1,48 +1,50 @@
 package yalter.mousetweaks.forge;
 
-import net.minecraftforge.api.distmarker.Dist;
-import net.minecraftforge.client.ConfigScreenHandler.ConfigScreenFactory;
 import net.minecraftforge.client.event.ScreenEvent.MouseButtonPressed;
 import net.minecraftforge.client.event.ScreenEvent.MouseButtonReleased;
 import net.minecraftforge.client.event.ScreenEvent.MouseDragged;
 import net.minecraftforge.client.event.ScreenEvent.MouseScrolled;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.fml.IExtensionPoint;
-import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.eventbus.api.bus.BusGroup;
+import net.minecraftforge.eventbus.api.listener.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.loading.FMLEnvironment;
 import yalter.mousetweaks.*;
+
+import java.lang.invoke.MethodHandles;
 
 @Mod(Constants.MOD_ID)
 public class MouseTweaksForge {
     public MouseTweaksForge() {
         Main.initialize();
-        MinecraftForge.EVENT_BUS.register(this);
+        BusGroup.DEFAULT.register(MethodHandles.lookup(), this);
 
         MinecraftForge.registerConfigScreen(ConfigScreen::new);
     }
 
     @SubscribeEvent
-    public void onGuiMouseClickedPre(MouseButtonPressed.Pre event) {
+    public boolean onGuiMouseClickedPre(MouseButtonPressed.Pre event) {
         Logger.DebugLog("onGuiMouseClickedPre button = " + event.getButton());
 
         MouseButton button = MouseButton.fromEventButton(event.getButton());
         if (button != null) {
             if (Main.onMouseClicked(event.getScreen(), event.getMouseX(), event.getMouseY(), button))
-                event.setCanceled(true);
+                return true;
         }
+
+        return false;
     }
 
     @SubscribeEvent
-    public void onGuiMouseReleasedPre(MouseButtonReleased.Pre event) {
+    public boolean onGuiMouseReleasedPre(MouseButtonReleased.Pre event) {
         Logger.DebugLog("onGuiMouseReleasedPre button = " + event.getButton());
 
         MouseButton button = MouseButton.fromEventButton(event.getButton());
         if (button != null) {
             if (Main.onMouseReleased(event.getScreen(), event.getMouseX(), event.getMouseY(), button))
-                event.setCanceled(true);
+                return true;
         }
+
+        return false;
     }
 
     @SubscribeEvent
@@ -57,7 +59,7 @@ public class MouseTweaksForge {
     }
 
     @SubscribeEvent
-    public void onGuiMouseDragPre(MouseDragged.Pre event) {
+    public boolean onGuiMouseDragPre(MouseDragged.Pre event) {
         // Sent when a mouse is dragged while a mouse button is down (so between Clicked and Released events). The
         // rate of reporting is high even when the FPS is limited through the options.
         Logger.DebugLog("onGuiMouseDragPre button = " + event.getMouseButton() + ", dx = " + event.getDragX() + ", dy = " + event.getDragY());
@@ -65,7 +67,9 @@ public class MouseTweaksForge {
         MouseButton button = MouseButton.fromEventButton(event.getMouseButton());
         if (button != null) {
             if (Main.onMouseDrag(event.getScreen(), event.getMouseX(), event.getMouseY(), button))
-                event.setCanceled(true);
+                return true;
         }
+
+        return false;
     }
 }
