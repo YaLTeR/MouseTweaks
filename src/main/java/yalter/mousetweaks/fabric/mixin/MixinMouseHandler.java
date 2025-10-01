2f765ef78a9191c9153942544bf3d5bcf83c3d4a
Abel <abelvanhulst@gmail.com>
2025-10-01 20:06:43 +0200

    Update Fabric to 1.21.9 but MixinMouseHandler & AbstractContainerScreenAccessor.getHoveredSlot() seem to throw errors when launching the game

--- src/main/java/yalter/mousetweaks/fabric/mixin/MixinMouseHandler.java
+++ src/main/java/yalter/mousetweaks/fabric/mixin/MixinMouseHandler.java
@@ -1,25 +1,26 @@
 package yalter.mousetweaks.fabric.mixin;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import net.minecraft.client.MouseHandler;
 import net.minecraft.client.gui.screens.Screen;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
+import net.minecraft.client.input.MouseButtonEvent;
 import yalter.mousetweaks.Main;
 import yalter.mousetweaks.MouseButton;
 
 @Mixin(MouseHandler.class)
 public abstract class MixinMouseHandler {
-    @WrapOperation(method = "handleAccumulatedMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/Screen;mouseDragged(DDIDD)Z"))
-    private boolean onMouseDragged(Screen screen, double x, double y, int activeButton, double dx, double dy, Operation<Boolean> operation) {
-        MouseButton button = MouseButton.fromEventButton(activeButton);
+    @WrapOperation(method = "handleAccumulatedMovement", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screens/inventory/Screen;mouseDragged(Lnet/minecraft/client/input/MouseButtonEvent;DD)Z"))
+    private boolean onMouseDragged(Screen screen, MouseButtonEvent mouseButtonEvent, double dx, double dy, Operation<Boolean> operation) {
+        MouseButton button = MouseButton.fromEventButton(mouseButtonEvent.button());
         if (button != null) {
-            if (Main.onMouseDrag(screen, x, y, button)) {
+            if (Main.onMouseDrag(screen, mouseButtonEvent.x(), mouseButtonEvent.y(), button)) {
                 return true;
             }
         }
 
-        return operation.call(screen, x, y, activeButton, dx, dy);
+        return operation.call(screen, mouseButtonEvent, dx, dy);
     }
 }
