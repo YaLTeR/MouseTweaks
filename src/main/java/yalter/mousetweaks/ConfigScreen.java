dad069cdddfd2c886030bf99eb4514a8348fa747
alchemyyy <iwjones@uw.edu>
2025-12-11 01:04:41 -0800

    Update to 1.21.11
    update to 1.21.11
    
    builds on all loaders. fabric version seems to work just fine.
    
    commands run:
    gradlew build
    gradlew -b build-fabric.gradle build
    gradlew -b build-neoforge.gradle build

--- src/main/java/yalter/mousetweaks/ConfigScreen.java
+++ src/main/java/yalter/mousetweaks/ConfigScreen.java
@@ -37,15 +37,16 @@ public class ConfigScreen extends Screen {
                 .create(this.width / 2 + 5, this.height / 6 + 24, 150, 20,
                         Component.literal("LMB Tweak Without Item"), (button, value) -> Main.config.lmbTweakWithoutItem = value));
 
+
         this.addRenderableWidget(
                 CycleButton.builder((WheelSearchOrder value) -> Component.literal(switch (value) {
                             case FIRST_TO_LAST -> "First to Last";
                             case LAST_TO_FIRST -> "Last to First";
-                        }))
+                        }), Main.config.wheelSearchOrder)
                         .withValues(WheelSearchOrder.FIRST_TO_LAST, WheelSearchOrder.LAST_TO_FIRST)
-                        .withInitialValue(Main.config.wheelSearchOrder)
                         .create(this.width / 2 - 155, this.height / 6 + 24 * 2, 310, 20,
                                 Component.literal("Wheel Tweak Search Order"), (button, value) -> Main.config.wheelSearchOrder = value));
+
 
         this.addRenderableWidget(
                 CycleButton.builder((WheelScrollDirection value) -> Component.literal(switch (value) {
@@ -53,9 +54,8 @@ public class ConfigScreen extends Screen {
                             case INVERTED -> "Up to Push, Down to Pull";
                             case INVENTORY_POSITION_AWARE -> "Inventory Position Aware";
                             case INVENTORY_POSITION_AWARE_INVERTED -> "Inventory Position Aware, Inverted";
-                        }))
+                        }), Main.config.wheelScrollDirection)
                         .withValues(WheelScrollDirection.NORMAL, WheelScrollDirection.INVERTED, WheelScrollDirection.INVENTORY_POSITION_AWARE, WheelScrollDirection.INVENTORY_POSITION_AWARE_INVERTED)
-                        .withInitialValue(Main.config.wheelScrollDirection)
                         .create(this.width / 2 - 155, this.height / 6 + 24 * 3, 310, 20,
                                 Component.literal("Scroll Direction"), (button, value) -> Main.config.wheelScrollDirection = value));
 
@@ -63,9 +63,8 @@ public class ConfigScreen extends Screen {
                 CycleButton.builder((ScrollItemScaling value) -> Component.literal(switch (value) {
                             case PROPORTIONAL -> "Multiple Wheel Clicks Move Multiple Items";
                             case ALWAYS_ONE -> "Always Move One Item (macOS Compatibility)";
-                        }))
+                        }), Main.config.scrollItemScaling)
                         .withValues(ScrollItemScaling.PROPORTIONAL, ScrollItemScaling.ALWAYS_ONE)
-                        .withInitialValue(Main.config.scrollItemScaling)
                         .create(this.width / 2 - 155, this.height / 6 + 24 * 4, 310, 20,
                                 Component.literal("Scroll Scaling"), (button, value) -> Main.config.scrollItemScaling = value));
 
